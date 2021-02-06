043d209e4b93a6bec58a12ae680c24a3eb5fc6c2
alanoud1alessa1 <anox1957@gmail.com>
2021-02-06 03:06:31 +0300

    Assignment 2

---
+++ app/src/main/java/com/example/it320softwareengineeringassignment2/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.it320softwareengineeringassignment2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    //when user click on the submit button
+    public void displayMessage(View view) {
+        EditText editText = (EditText) findViewById(R.id.editText);
+        String message = "Hello , "+editText.getText().toString();
+
+        TextView textView = findViewById(R.id.textView);
+        textView.setText(message);
+    }
+
+}
